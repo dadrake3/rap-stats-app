9d9cbea471dc1f0aaefdba4f1bd5918033f4d40d
Daryl Drake <dadrake3@gmail.com>
2019-02-12 03:33:10 -0600

    Initial app boilerplate

--- app.py
+++ app.py
@@ -1,29 +1,29 @@
-import os
 import dash
-import dash_core_components as dcc
 import dash_html_components as html
+import dash_core_components as dcc
+import requests
 
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
-app = dash.Dash(__name__)
-server = app.server
-
-
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 app.layout = html.Div([
-    html.H2('Hello World'),
-    dcc.Dropdown(
-        id='dropdown',
-        options=[{'label': i, 'value': i} for i in ['LA', 'NYC', 'MTL']],
-        value='LA'
-    ),
-    html.Div(id='display-value')
+    # html.Div(dcc.Input(id='input-box', type='text')),
+    html.Button('Get Artist', id='artist_btn'),
+
+    html.Div(id='output-container',
+             children='Enter a value and press submit')
 ])
 
-app.css.append_css({"external_url": "https://codepen.io/chriddyp/pen/bWLwgP.css"})
 
+@app.callback(
+    dash.dependencies.Output('output-container', 'children'),
+    [dash.dependencies.Input('artist_btn', 'n_clicks')],
+    # [dash.dependencies.State('input-box', 'value')]
+    )
+def update_output(n_clicks, value=20503):
+	# print(requests.get(f'https://rap-stats.com/Markov?artist_id=20503').text)
+	return requests.get(f'https://rap-stats.com/Markov?artist_id={value}').json()['data']
 
-@app.callback(dash.dependencies.Output('display-value', 'children'), [dash.dependencies.Input('dropdown', 'value')])
-def display_value(value):
-    return 'You have selected "{}"'.format(value)
 
 
 if __name__ == '__main__':
